27b5fbc98d5152d946663cc6fbbba6878a109a00
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-28 23:03:50 -0500

    Starting with ruby

---
+++ first_ruby.rb
@@ -0,0 +1,15 @@
+puts "HOLA MUNDO DESDE UN SCRIPT"
+
+# UTILIZANDO OBJETOS EN RUBY
+
+hola = String.new("hola") # => "hola"
+
+puts hola #Ejecutar un metodo de un objeto en particular
+
+a = 54 
+
+puts a.next # => 55
+
+
+
+system("pause")
