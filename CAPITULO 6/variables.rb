d226a8c5259b49ef958ae6ab4955b4c9ad548253
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-29 00:16:02 -0500

    Asignaciones abreviadas

--- CAPITULO 6/variables.rb
+++ CAPITULO 6/variables.rb
@@ -12,10 +12,24 @@ puts nombre + " " + primer_apellido + " " + segundo_apellido # => Fulano Sutano 
 #TIPADO DINÁMICO
 
 información_personal = "Fulano Zutano Perengano"
-puts información_personal.class
+puts información_personal.class # => string
 
 información_personal = 26
-puts información_personal.class
+puts información_personal.class # => integer
 
 información_personal= 1.72
-puts información_personal.class
+puts información_personal.class # => float
+
+#ASIGNACIONES ABREVIADAS
+
+decremento = 12
+decremento -= 1
+
+puts decremento  # => 11
+
+productos_en_almacen = 72
+productos_vendidos = 12
+
+productos_en_almacen -= productos_vendidos
+
+puts productos_en_almacen  # => 60
