3796725a121ce501f0e9c68d5a6b12cbdda4b825
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-31 15:11:40 -0500

    Relational operators

---
+++ CAP 7 LOS NÚMEROS/operadores_relacionales.rb
@@ -0,0 +1,10 @@
+num1 = 7
+num2 = 8
+
+puts num1 > num2 #=> false
+puts num1 < num2 #=> true
+puts num1 == num2 # => false
+puts num1 <= num2 #=> true
+puts num1 >= num2 #=> false
+puts num1 != num2 #=> true
+
