467f9a7578e48ee88e0c896baec7733b9d4cc795
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-30 20:53:51 -0500

    floats

---
+++ CAP 7 LOS NÚMEROS/numeros_flotantes.rb
@@ -0,0 +1,16 @@
+flotante_decimal =0.0
+
+puts flotante_decimal.class
+
+flotante_millonesimal = 4.234516
+
+puts flotante_decimal.class
+
+flotante_notación = 2.5e45
+
+puts flotante_notación.class
+
+#TRUNCATE AND ROUND
+
+puts 28.8920134.truncate(2)
+puts 3.772.round(1)
---
+++ CAP 7 LOS NÚMEROS/números_enteros.rb
@@ -0,0 +1,6 @@
+entero_negativo = -68
+
+puts entero_negativo.class
+puts 12_342_333_320_111
+
+
