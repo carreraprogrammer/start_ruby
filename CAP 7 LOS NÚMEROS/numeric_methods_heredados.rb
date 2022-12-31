8c6d308a1aa31b2248049e3df38860f670af09e1
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-31 14:51:43 -0500

    numeric methods

---
+++ CAP 7 LOS NÚMEROS/numeric_methods_heredados.rb
@@ -0,0 +1,42 @@
+cantidad = -1
+
+puts cantidad.zero? #=> false
+puts cantidad.negative? #=> true
+puts cantidad.positive? #=> false
+
+#Suma
+
+puts 10 + 2 # => 12
+
+#Resta
+
+puts 10 - 2 # => 8
+
+#Multiplicación
+
+puts 10 * 2 # => 20
+
+#División
+
+puts 10 / 2 # => 5
+
+#Potencia
+
+puts 10 ** 2 # => 100
+
+#modulo
+
+puts 10 % 2 # => 0
+
+#Division entera
+
+division_entera = 12 / 5
+
+puts division_entera # => 5 because all numbers are integers
+
+#Division real
+
+division_real = 12 / 5.0
+
+puts division_real # => 2.4 because there are unleass one float number
+
