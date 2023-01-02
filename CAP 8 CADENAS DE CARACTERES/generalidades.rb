e2b144ff8caf418d770abca23ebb7cdf955819f5
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-02 13:09:33 -0500

    Chains generalities

---
+++ CAP 8 CADENAS DE CARACTERES/generalidades.rb
@@ -0,0 +1,7 @@
+puts "CADENA DE COMMILLAS DOBLES".class #=> string
+puts 'CADENA DE COMILLAS SIMPLES'.class #=> string
+
+# Cada caracter tiene una posision
+example = 'Coleccion de caracteres'
+
+puts example[0]
