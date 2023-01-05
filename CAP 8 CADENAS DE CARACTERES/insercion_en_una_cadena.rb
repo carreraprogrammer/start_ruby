c8111397422642203b54381c6dfd9ccb84286dfe
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-04 20:21:36 -0500

    Insert chain

---
+++ CAP 8 CADENAS DE CARACTERES/insercion_en_una_cadena.rb
@@ -0,0 +1,10 @@
+
+p "Uno, dos, , cuatro, cinco".insert(10,"tres")  #=> "Uno, dos, tres, cuatro, cinco"
+
+insercion_dia = "Hoy es: "
+insercion_dia.insert(8, "jueves")
+p insercion_dia
+
+#Prepend
+
+p "Francisco".prepend("Sr.") #=> Sr. Francisco
