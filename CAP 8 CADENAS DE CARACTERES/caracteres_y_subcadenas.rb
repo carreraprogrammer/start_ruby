942f4bb8d0f080f89904d3d25db1fa572242f4db
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-04 20:12:16 -0500

    Objects and chains

---
+++ CAP 8 CADENAS DE CARACTERES/caracteres_y_subcadenas.rb
@@ -0,0 +1,15 @@
+#índice
+
+caracteres = "caracteres"
+
+puts caracteres[0] #=> c
+
+#Subcadena
+
+puts "fragmento de una cadena"[0..8] #=> fragmento
+
+puts "fragmento de una cadena"[0,9] #=> fragmento
+
+puts "fragmento de una cadena".slice(0..8) #=> fragmento
+
+puts "fragmento de una cadena".slice(0,9) #=> fragmento
