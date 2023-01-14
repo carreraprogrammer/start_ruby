90c949e449c3f57a8abaf4babb7b4f3b9d5efe22
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-13 23:02:56 -0500

    Finish Hashes

--- CAP 10 COLECCIONES DE OBJETOS/los_hashes.rb
+++ CAP 10 COLECCIONES DE OBJETOS/los_hashes.rb
@@ -57,4 +57,59 @@ puts direccion.length #=> 9
 puts direccion.key? (:municipio) #=> true
 puts direccion.key?(:cp) #=> false
 
-#
+# OBTENCION DE CLAVES Y VALORES
+
+datos_de_contacto = {
+    nombre: "Fulano",
+    telefono: "0000000000",
+    email: "fulanito@gmail.com"
+}
+
+## Obtencion de una sola clave o valor
+
+print datos_de_contacto[:nombre] #=> Fulano
+puts ''
+print datos_de_contacto.key("Fulano") #=> nombre
+puts ''
+
+## Obtención de varias claves o valores
+
+print datos_de_contacto.values #=> ["Fulano", "0000000000", "fulanito@gmail.com"]
+puts ''
+print datos_de_contacto.values_at(:nombre, :email) #=> ["Fulano", "fulanito@gmail.com"]
+puts ''
+print datos_de_contacto.keys # => [:nombre, :telefono, :email]
+puts ''
+
+# MODIFICACIÓN DE LOS HASHES
+
+## Inserciones de clave-valor y sustitución de valores
+
+capitales_america_norte = {'Canada' => 'Ottawa'}
+print capitales_america_norte #=> {"Canada"=>"Ottawa"}
+puts ''
+capitales_america_norte["México"] = 'Distrito Federal'
+print capitales_america_norte #=> {"Canada"=>"Ottawa", "México"=>"Distrito Federal"}
+puts ''
+capitales_america_norte['México'] = 'Ciudad de México'
+print capitales_america_norte #=> {"Canada"=>"Ottawa", "México"=>"Ciudad de México"}
+puts ''
+capitales_america_norte.store('Estados Unidos', 'Washintong D.C')
+print capitales_america_norte #=> {"Canada"=>"Ottawa", "México"=>"Ciudad de México", "Estados Unidos"=>"Washintong D.C"}
+puts ''
+
+## Combinación de hashes
+
+capitales_america_central = {'Honduras' => 'Tegucigalpa', 'El Salvador' => 'San Salvador', 'Guatemala' => 'Ciudad de Guatemala'}
+
+capitales_america_norte.merge!(capitales_america_central)
+
+print capitales_america_norte #=> {"Canada"=>"Ottawa", "México"=>"Ciudad de México", "Estados Unidos"=>"Washintong D.C", "Honduras"=>"Tegucigalpa", "El Salvador"=>"San Salvador", "Guatemala"=>"Ciudad de Guatemala"}
+puts ''
+
+## Eliminación a través de las claves
+
+capitales_america_norte.delete('El Salvador')
+
+print capitales_america_norte #=> {"Canada"=>"Ottawa", "México"=>"Ciudad de México", "Estados Unidos"=>"Washintong D.C", "Honduras"=>"Tegucigalpa", "Guatemala"=>"Ciudad de Guatemala"}
+
